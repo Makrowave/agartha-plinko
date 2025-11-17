bf417c05d5e160eabc1d59c40e686554e5e7a9ea
Makrowave <maksstyzej@gmail.com>
2025-11-17 19:55:07 +0100

    Initial commit

---
+++ agartha-plinko-backend/src/main/java/org/makrowave/agartha_plinko_backend/AgarthaPlinkoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.makrowave.agartha_plinko_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgarthaPlinkoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgarthaPlinkoBackendApplication.class, args);
+	}
+
+}
---
+++ agartha-plinko-backend/src/test/java/org/makrowave/agartha_plinko_backend/AgarthaPlinkoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.makrowave.agartha_plinko_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgarthaPlinkoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
