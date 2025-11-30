24c808ea6cdfa3b7c8a8bd59db6dc41b32556370
Makrowave <maksstyzej@gmail.com>
2025-11-30 14:40:23 +0100

    feat: Add email validation and change password

--- agartha-plinko-backend/src/main/java/org/makrowave/agartha_plinko_backend/authentication/service/AuthService.java
+++ agartha-plinko-backend/src/main/java/org/makrowave/agartha_plinko_backend/authentication/service/AuthService.java
@@ -2,9 +2,12 @@ package org.makrowave.agartha_plinko_backend.authentication.service;
 
 import lombok.RequiredArgsConstructor;
 import org.makrowave.agartha_plinko_backend.shared.domain.model.User;
+import org.makrowave.agartha_plinko_backend.shared.util.Validation;
 import org.makrowave.agartha_plinko_backend.user.repository.IUserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.math.BigDecimal;
 
@@ -15,13 +18,18 @@ public class AuthService implements IAuthService {
     private final IUserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    @Override
     public User register(String username, String email, String rawPassword) {
         if (userRepository.findByUsername(username).isPresent()) {
-            throw new RuntimeException("Username already exists");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username already exists");
+        }
+
+        if (!Validation.EMAIL_REGEX.matcher(email).matches()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid email format");
         }
 
         if (userRepository.findByEmail(email).isPresent()) {
-            throw new RuntimeException("Email already exists");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email already exists");
         }
 
         User user = User.builder()
@@ -32,5 +40,18 @@ public class AuthService implements IAuthService {
                 .build();
 
         return userRepository.save(user);
+    }
+
+    @Override
+    public void changePassword(Long userId, String oldPassword, String newPassword) {
+        var user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
+
+        if (!passwordEncoder.matches(oldPassword, user.getPassword())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid current password");
+        }
+
+        user.setHash(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
     }
 }
--- agartha-plinko-backend/src/main/java/org/makrowave/agartha_plinko_backend/authentication/service/IAuthService.java
+++ agartha-plinko-backend/src/main/java/org/makrowave/agartha_plinko_backend/authentication/service/IAuthService.java
@@ -4,4 +4,6 @@ import org.makrowave.agartha_plinko_backend.shared.domain.model.User;
 
 public interface IAuthService {
     User register(String username, String email, String rawPassword);
+
+    public void changePassword(Long userId, String oldPassword, String newPassword);
 }
