6aa59eaad334909563fbe9b105fbae3f2118bac7
Makrowave <maksstyzej@gmail.com>
2025-12-03 16:44:25 +0100

    feat: Add tests for Billing Address

--- agartha-plinko-backend/src/main/java/org/makrowave/agartha_plinko_backend/authentication/domain/RegisterDto.java
+++ agartha-plinko-backend/src/main/java/org/makrowave/agartha_plinko_backend/authentication/domain/RegisterDto.java
@@ -1,7 +1,9 @@
 package org.makrowave.agartha_plinko_backend.authentication.domain;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record RegisterDto(
-        String username,
-        String email,
-        String password
+        @NotBlank String username,
+        @NotBlank String email,
+        @NotBlank String password
 ) {}
